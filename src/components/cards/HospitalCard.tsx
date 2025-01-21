867793a84fe2726c37268db9d5b6d0b6a0dcbaaa
jnakaso <jnakaso@yahoo.com>
2025-01-21 10:51:37 -0800

    fix popop

--- src/components/cards/HospitalCard.tsx
+++ src/components/cards/HospitalCard.tsx
@@ -99,179 +99,161 @@ export const HospitalCard: React.FC<HospitalCardProps> = ({
   }, []);
 
   return (
-    <>
-      <style>
-        {`
-      .carousel .control-arrow:hover {
-        background: none !important; 
-        box-shadow: none !important; 
-      }
-      .carousel .control-prev.control-arrow {
-        left: 30px;
-        }
-      .carousel .control-next.control-arrow {
-        right: 30px;
-        }
-    `}
-      </style>
-      <Card
+    <Card
+      sx={{
+        height: "242px",
+        borderRadius: "10px",
+        boxShadow: "0px 14px 80px rgba(34, 35, 58, 0.2)",
+      }}
+    >
+      <Box className="media-container">
+        {popupInfo && popupInfo.hospital.hospitalPictures.length > 0 && (
+          <>
+            <Chip
+              icon={<LocationOnIcon />}
+              label={`${popupInfo?.hospital.city},${popupInfo?.hospital.state}`}
+              sx={{
+                "& .MuiChip-icon": {
+                  color: markerColor,
+                  fontSize: "15px",
+                },
+                fontSize: "8px",
+                height: "auto",
+              }}
+              className="chip"
+              size="small"
+            />
+            <div className="close-btn-container">
+              <CustomCancelIconButton
+                aria-label="close"
+                className="close-btn"
+                onClick={onClose}
+              >
+                <CancelRoundedIcon />
+              </CustomCancelIconButton>
+            </div>
+
+            <Carousel
+              showStatus={false}
+              showThumbs={false}
+              showIndicators={false}
+            >
+              {popupInfo?.hospital.hospitalPictures.map((url, idx) => (
+                <CardMedia
+                  key={"p" + idx}
+                  component="img"
+                  sx={{ height: 90 }}
+                  image={url}
+                  alt="Hospital Image"
+                />
+              ))}
+            </Carousel>
+          </>
+        )}
+      </Box>
+      <Box>
+        <Typography sx={{ fontSize: "10px" }}>
+          <Box
+            component="span"
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            sx={{
+              backgroundColor: (theme: Theme) => theme.palette.background.highlighted,
+              width: "265px",
+              height: "20px",
+              visibility:
+                popupInfo?.hospital.status === "active"
+                  ? "visible"
+                  : "hidden",
+            }}
+          >
+            Matched by {partnerName}
+            {/* <CustomAvatar src="/path/to/profile1.jpg" />
+            <CustomAvatar src="/path/to/profile2.jpg" />+ */}
+          </Box>
+        </Typography>
+      </Box>
+      <CardContent
         sx={{
-          width: "265px",
-          height: "242px",
-          border: "none",
-          borderRadius: "10px",
-          boxShadow: "0px 14px 80px rgba(34, 35, 58, 0.2)",
+          padding:
+            popupInfo?.hospital.status === "active"
+              ? "21px 16px 8px 16px"
+              : "8px 16px",
+          marginTop: "-10px",
         }}
       >
-        <Box className="media-container">
-          {popupInfo && popupInfo.hospital.hospitalPictures.length > 0 && (
-            <>
-              <Chip
-                icon={<LocationOnIcon />}
-                label={`${popupInfo?.hospital.city},${popupInfo?.hospital.state}`}
-                sx={{
-                  "& .MuiChip-icon": {
-                    color: markerColor,
-                    fontSize: "15px",
-                  },
-                  fontSize: "8px",
-                  height: "auto",
-                }}
-                className="chip"
-                size="small"
-              />
-              <div className="close-btn-container">
-                <CustomCancelIconButton
-                  aria-label="close"
-                  className="close-btn"
-                  onClick={onClose}
-                >
-                  <CancelRoundedIcon />
-                </CustomCancelIconButton>
-              </div>
+        <Typography gutterBottom component="div" sx={{ fontSize: "14px" }}>
+          {popupInfo?.hospital.name}
+        </Typography>
 
-              <Carousel
-                showStatus={false}
-                showThumbs={false}
-                showIndicators={false}
-              >
-                {popupInfo?.hospital.hospitalPictures.map((url, idx) => (
-                  <CardMedia
-                    key={"p" + idx}
-                    component="img"
-                    sx={{ height: 90 }}
-                    image={url}
-                    alt="Hospital Image"
-                  />
-                ))}
-              </Carousel>
-            </>
-          )}
-        </Box>
-        <Box>
-          <Typography sx={{ fontSize: "10px" }}>
-            <Box
-              component="span"
-              display="flex"
-              alignItems="center"
-              justifyContent="center"
-              sx={{
-                backgroundColor: (theme: Theme) => theme.palette.background.highlighted,
-                width: "265px",
-                height: "20px",
-                visibility:
-                  popupInfo?.hospital.status === "active"
-                    ? "visible"
-                    : "hidden",
-              }}
-            >
-              Matched by {partnerName}
-              {/* <CustomAvatar src="/path/to/profile1.jpg" />
-            <CustomAvatar src="/path/to/profile2.jpg" />+ */}
-            </Box>
-          </Typography>
-        </Box>
-        <CardContent
-          sx={{
-            padding:
-              popupInfo?.hospital.status === "active"
-                ? "21px 16px 8px 16px"
-                : "8px 16px",
-            marginTop: "-10px",
-          }}
+        <Typography color="text.secondary" sx={{ fontSize: "10px" }}>
+          ${popupInfo?.hospital.matchedFunded?.fundingCompleted || 0}{" "}
+          raised of ${popupInfo?.hospital.matchedRequest?.requested} -{" "}
+          <EmphasizedText
+            sx={{
+              color: theme.palette.hospital.open,
+              fontSize: "10px"
+            }}>
+            {popupInfo?.hospital.status === "active" && "Actively Funding"}
+          </EmphasizedText>
+        </Typography>
+        <Typography sx={{ fontSize: "10px" }}>
+          {popupInfo?.hospital.year}+ kids impacted
+        </Typography>
+
+        <Stack
+          direction="row"
+          marginTop={"8px"}
+          gap={1}
+          paddingTop={
+            popupInfo?.hospital.status === "active" ? "0px" : "19px"
+          }
         >
-          <Typography gutterBottom component="div" sx={{ fontSize: "14px" }}>
-            {popupInfo?.hospital.name}
-          </Typography>
-
-          <Typography color="text.secondary" sx={{ fontSize: "10px" }}>
-            ${popupInfo?.hospital.matchedFunded?.fundingCompleted || 0}{" "}
-            raised of ${popupInfo?.hospital.matchedRequest?.requested} -{" "}
-            <EmphasizedText
-              sx={{
-                color: theme.palette.hospital.open,
-                fontSize: "10px"
-              }}>
-              {popupInfo?.hospital.status === "active" && "Actively Funding"}
-            </EmphasizedText>
-          </Typography>
-          <Typography sx={{ fontSize: "10px" }}>
-            {popupInfo?.hospital.year}+ kids impacted
-          </Typography>
-
-          <Stack
-            direction="row"
-            marginTop={"8px"}
-            gap={1}
-            paddingTop={
-              popupInfo?.hospital.status === "active" ? "0px" : "19px"
-            }
+          <ActionButton
+            sx={{
+              height: "26px",
+              borderRadius: "10px",
+              fontSize: "10px",
+            }}
+            onClick={(evt: any) => {
+              evt.stopPropagation();
+              setLearnMoreHospital(popupInfo?.hospital);
+            }}
+            s
           >
-            <ActionButton
-              sx={{
-                height: "26px",
-                borderRadius: "10px",
-                fontSize: "10px",
-              }}
-              onClick={(evt: any) => {
-                evt.stopPropagation();
-                setLearnMoreHospital(popupInfo?.hospital);
-              }}
-              s
-            >
-              Learn more
-            </ActionButton>
-            <ActionButton
-              disabled={!isOpen}
-              sx={{
-                height: "26px",
-                borderRadius: "10px",
-                fontSize: "10px",
-              }}
-              onClick={(evt: any) => {
-                evt.stopPropagation();
-                setDonationHospital(popupInfo?.hospital);
-              }}
-            >
-              Donate
-            </ActionButton>
-          </Stack>
-          <Box sx={{ marginBottom: "5px" }}>
-            <Typography
-              textAlign={"center"}
-              sx={{
-                marginTop:
-                  popupInfo?.hospital.status === "active" ? "8px" : "5px",
-                fontSize: "10px",
-                color: "grey",
-                fontWeight: "bold",
-              }}
-            >
-              {getDonationMessage()}
-            </Typography>
-          </Box>
-        </CardContent>
-      </Card>
-    </>
+            Learn more
+          </ActionButton>
+          <ActionButton
+            disabled={!isOpen}
+            sx={{
+              height: "26px",
+              borderRadius: "10px",
+              fontSize: "10px",
+            }}
+            onClick={(evt: any) => {
+              evt.stopPropagation();
+              setDonationHospital(popupInfo?.hospital);
+            }}
+          >
+            Donate
+          </ActionButton>
+        </Stack>
+        <Box sx={{ marginBottom: "5px" }}>
+          <Typography
+            textAlign={"center"}
+            sx={{
+              marginTop:
+                popupInfo?.hospital.status === "active" ? "8px" : "5px",
+              fontSize: "10px",
+              color: "grey",
+              fontWeight: "bold",
+            }}
+          >
+            {getDonationMessage()}
+          </Typography>
+        </Box>
+      </CardContent>
+    </Card>
   );
 };
