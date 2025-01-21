23fc78155377c79bccd9a421d0b5089ceb387be0
Sushmita Ghosh <sushmita.ghosh@scamptech.com>
2025-01-21 09:20:55 -0800

    [Sushmita][Added mapping for Title Request section]

--- src/components/HospitalPage/HospitalPageTitleRequestNarrative.tsx
+++ src/components/HospitalPage/HospitalPageTitleRequestNarrative.tsx
@@ -1,5 +1,8 @@
 import { Box, Grid, LinearProgress, Typography } from "@mui/material";
 import { styled } from "@mui/system";
+import { LearnMoreHospitalContext } from "../../context/SelectedHospitalContext";
+import { useContext } from "react";
+import DonationBarActive from "./DonationBarActive";
 
 const DonationsRecievedProgressBar = styled(LinearProgress)({
   height: 40,
@@ -10,133 +13,136 @@ const DonationsRecievedProgressBar = styled(LinearProgress)({
 });
 
 const HospitalPageTitleRequestNarrative = () => {
+  const { hospital } = useContext(LearnMoreHospitalContext);
   return (
-    <Box sx={{ padding: "20px" }}>
-      {/* Title Request Narrative & Request Narrative Section */}
-      <Grid
-        container
-        spacing={4}
-        sx={{
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
+    <>
+      {/* Donations Received Section - active hospital */}
+      {hospital?.status === "past" && (
+        <Grid item>
+          <DonationBarActive />
+        </Grid>
+      )}
+      <Box sx={{ padding: "20px" }}>
+        {/* Title Request Narrative & Request Narrative Section */}
         <Grid
-          item
-          xs={12}
-          md={6}
+          container
+          spacing={4}
           sx={{
-            padding: "20px",
             justifyContent: "center",
             alignItems: "center",
           }}
         >
-          <Grid item>
-            <Typography variant="h6" gutterBottom sx={{ color: "#8A8A8A" }}>
-              Title Request Narrative
-            </Typography>
-            <Typography variant="body1" gutterBottom>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </Typography>
-          </Grid>
-
-          <Grid item>
-            <Typography variant="h6" gutterBottom sx={{ color: "#8A8A8A" }}>
-              Request Narrative
-            </Typography>
-            <Typography variant="body1" mt={2}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </Typography>
-          </Grid>
-        </Grid>
-
-        <Grid item xs={12} md={6}>
-          <Grid item>
-            {/* Donations Received Section */}
-            <Box mt={2}>
-              <Typography variant="h6" sx={{ fontSize: "20px" }}>
-                Donations Received:
-                <span>150k raised out of 100k (150%)</span>
+          <Grid
+            item
+            xs={12}
+            md={6}
+            sx={{
+              padding: "20px",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Grid item>
+              <Typography
+                variant="body1"
+                gutterBottom
+                sx={{ color: "#8A8A8A" }}
+              >
+                {hospital?.matchedRequest?.titleRequestNarrative}
               </Typography>
+            </Grid>
 
-              <Box mt={2}>
-                <DonationsRecievedProgressBar
-                  variant="determinate"
-                  value={100}
-                />
+            <Grid item>
+              <Typography variant="body1" gutterBottom>
+                {hospital?.matchedRequest?.requestNarrative}
+              </Typography>
+            </Grid>
+          </Grid>
+
+          <Grid item xs={12} md={6}>
+            <Grid item>
+              {/* Donations Received Section - past hospital */}
+              {hospital?.status === "active" && (
+                <Box mt={2}>
+                  <Typography variant="h6" sx={{ fontSize: "20px" }}>
+                    Donations Received:
+                    <span>150k raised out of 100k (150%)</span>
+                  </Typography>
+
+                  <Box mt={2}>
+                    <DonationsRecievedProgressBar
+                      variant="determinate"
+                      value={100}
+                    />
+                  </Box>
+                </Box>
+              )}
+
+              {/* Brand Partners Section */}
+              <Box mt={4}>
+                <Typography variant="h6" gutterBottom>
+                  Brand Partners
+                </Typography>
+                <Grid container spacing={4} alignItems="center">
+                  <Grid item xs={3}>
+                    <img
+                      src="https://via.placeholder.com/100" // Replace with actual image
+                      alt="Partner Logo"
+                      style={{ width: "100%", height: "auto", borderRadius: 8 }}
+                    />
+                  </Grid>
+                  <Grid item xs={9}>
+                    <Box
+                      sx={{
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "space-between",
+                      }}
+                    >
+                      <Typography variant="h6">Partner Name</Typography>
+                      <Typography variant="body2" mt={1}>
+                        Match Amount: $25,000
+                      </Typography>
+                    </Box>
+                    <Typography variant="body2" mt={1}>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua.
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={3}>
+                    <img
+                      src="https://via.placeholder.com/100" // Replace with actual image
+                      alt="Partner Logo"
+                      style={{ width: "100%", height: "auto", borderRadius: 8 }}
+                    />
+                  </Grid>
+                  <Grid item xs={9}>
+                    <Box
+                      sx={{
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "space-between",
+                      }}
+                    >
+                      <Typography variant="h6">Partner Name</Typography>
+                      <Typography variant="body2" mt={1}>
+                        Match Amount: $25,000
+                      </Typography>
+                    </Box>
+                    <Typography variant="body2" mt={1}>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua.
+                    </Typography>
+                  </Grid>
+                </Grid>
               </Box>
-            </Box>
-            {/* Brand Partners Section */}
-            <Box mt={4}>
-              <Typography variant="h6" gutterBottom>
-                Brand Partners
-              </Typography>
-              <Grid container spacing={4} alignItems="center">
-                <Grid item xs={3}>
-                  <img
-                    src="https://via.placeholder.com/100" // Replace with actual image
-                    alt="Partner Logo"
-                    style={{ width: "100%", height: "auto", borderRadius: 8 }}
-                  />
-                </Grid>
-                <Grid item xs={9}>
-                  <Box
-                    sx={{
-                      display: "flex",
-                      alignItems: "center",
-                      justifyContent: "space-between",
-                    }}
-                  >
-                    <Typography variant="h6">Partner Name</Typography>
-                    <Typography variant="body2" mt={1}>
-                      Match Amount: $25,000
-                    </Typography>
-                  </Box>
-                  <Typography variant="body2" mt={1}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua.
-                  </Typography>
-                </Grid>
-                <Grid item xs={3}>
-                  <img
-                    src="https://via.placeholder.com/100" // Replace with actual image
-                    alt="Partner Logo"
-                    style={{ width: "100%", height: "auto", borderRadius: 8 }}
-                  />
-                </Grid>
-                <Grid item xs={9}>
-                  <Box
-                    sx={{
-                      display: "flex",
-                      alignItems: "center",
-                      justifyContent: "space-between",
-                    }}
-                  >
-                    <Typography variant="h6">Partner Name</Typography>
-                    <Typography variant="body2" mt={1}>
-                      Match Amount: $25,000
-                    </Typography>
-                  </Box>
-                  <Typography variant="body2" mt={1}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua.
-                  </Typography>
-                </Grid>
-              </Grid>
-            </Box>
+            </Grid>
           </Grid>
         </Grid>
-      </Grid>
-    </Box>
+      </Box>
+    </>
   );
 };
 
