df4196eadde690f63763f71874c44c28ae7ffde5
Sushmita Ghosh <sushmita.ghosh@scamptech.com>
2025-01-21 09:17:08 -0800

    [Sushmita][Display similar sections only for past hospitals]

--- src/components/HospitalPage/HospitalDetailsPageModal.tsx
+++ src/components/HospitalPage/HospitalDetailsPageModal.tsx
@@ -5,15 +5,18 @@ import HospitalPageStatsSection from "./HospitalPageStatsSection";
 import HospitalPageMain from "./HospitalPageMain";
 import HospitalPageSimilarDetailsSection from "./HospitalPageSimilarDetailsSection";
 import HospitalPageTitleRequestNarrative from "./HospitalPageTitleRequestNarrative";
+import { useContext } from "react";
+import { LearnMoreHospitalContext } from "../../context/SelectedHospitalContext";
 
 const HospitalDetailsPageModal = () => {
+  const { hospital } = useContext(LearnMoreHospitalContext);
   return (
     <Box sx={{ padding: "20px" }}>
       <HospitalPageMain />
       <HospitalPageTitleRequestNarrative />
       <HospitalPageStatsSection />
       <HospitalPageContent />
-      <HospitalPageSimilarDetailsSection />
+      {hospital?.status === "past" && <HospitalPageSimilarDetailsSection />}
       <HospitalPageFooter />
     </Box>
   );
