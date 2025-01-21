1b84aa7a6a257f046ab8daa5e2d30312a8561c7a
Sushmita Ghosh <sushmita.ghosh@scamptech.com>
2025-01-21 09:20:24 -0800

    [Sushmita][Added mapping for impact section, thank you section only visible for past hospitals]

--- src/components/HospitalPage/HospitalPageContent.tsx
+++ src/components/HospitalPage/HospitalPageContent.tsx
@@ -8,49 +8,49 @@ const HospitalPageContent = () => {
   return (
     <div>
       {/* Thank You Section */}
-      <HospitalPageBaseComponent
-        header={hospital?.matchedFunded?.thankYouNoteTitle}
-        shortParagraph={hospital?.matchedFunded?.shortThankYou}
-        paragraph={hospital?.matchedFunded?.thankYouNote}
-        styles={{
-          container: { backgroundColor: "#f9f9f9", padding: "6rem" },
-          header: {
-            color: "#333",
-            textAlign: "center",
-            fontSize: "2rem",
-            fontWeight: "bold",
-          },
-          shortParagraph: { color: "#828282", textAlign: "center" },
-          paragraph: { color: "#000", textAlign: "left" },
-        }}
-      />
+      {hospital?.status === "past" && hospital?.matchedFunded && (
+        <HospitalPageBaseComponent
+          header={hospital?.matchedFunded?.thankYouNoteTitle}
+          shortParagraph={hospital?.matchedFunded?.shortThankYou}
+          paragraph={hospital?.matchedFunded?.thankYouNote}
+          styles={{
+            container: { backgroundColor: "#f9f9f9", padding: "6rem" },
+            header: {
+              color: "#333",
+              textAlign: "center",
+              fontSize: "2rem",
+              fontWeight: "bold",
+            },
+            shortParagraph: { color: "#828282", textAlign: "center" },
+            paragraph: { color: "#000", textAlign: "left" },
+          }}
+        />
+      )}
 
       {/* Impact Section */}
-      <HospitalPageBaseComponent
-        image="https://via.placeholder.com/345x180"
-        header={hospital?.matchedFunded?.impactTitle}
-        shortParagraph={"This is a short paragraph about the impact."}
-        paragraph={`This is the positive impact made possible with your donations!
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`}
-        styles={{
-          container: { backgroundColor: "#039BDC", padding: "5rem" },
-          imageContainer: {},
-          header: { color: "#fff", textAlign: "center", fontSize: "2rem" },
-          paragraph: { color: "#fff" },
-          shortParagraph: { color: "#F3C941" },
-        }}
-      />
+      {hospital?.status === "past" && hospital?.matchedFunded && (
+        <HospitalPageBaseComponent
+          image={hospital?.matchedFunded?.impactPictures[0]}
+          header={hospital?.matchedFunded?.impactTitle}
+          shortParagraph={
+            "This is the positive impact made possible with your donations!"
+          }
+          paragraph={hospital?.matchedFunded?.impactText}
+          styles={{
+            container: { backgroundColor: "#039BDC", padding: "5rem" },
+            imageContainer: {},
+            header: { color: "#fff", textAlign: "left", fontSize: "2rem" },
+            paragraph: { color: "#fff", textAlign: "left" },
+            shortParagraph: { color: "#F3C941", paddingBottom: "1rem" },
+          }}
+        />
+      )}
 
       {/* About Our Health Organization Section */}
       <HospitalPageBaseComponent
         header="About Our Health Organization"
-        shortParagraph={`This is a short paragraph about our health organization.`}
-        paragraph={`Our mission at the organization: 
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`}
+        shortParagraph={`Our mission at the organization`}
+        paragraph={hospital?.description}
         styles={{
           container: { backgroundColor: "#f9f9f9", padding: "5rem" },
           header: {
