712b7afb3f0d63e41d987fed349312d4d30174ea
Sushmita Ghosh <sushmita.ghosh@scamptech.com>
2025-01-21 09:27:34 -0800

    [Sushmita][Fixed Es lint issues]

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -10,21 +10,21 @@
  * @returns
  */
 export const daysRemaining = (date: string | Date): number => {
-  let date1 = new Date();
+  const date1 = new Date();
 
   // if date is undefined, throw error
   if (!date) {
     throw new Error("Invalid date format");
   }
 
-  let date2 = typeof date === "string" ? new Date(date) : date;
+  const date2 = typeof date === "string" ? new Date(date) : date;
   // One day Time in ms (milliseconds)
-  let oneDay = 1000 * 60 * 60 * 24;
+  const oneDay = 1000 * 60 * 60 * 24;
 
   // Calculate the number of days between two dates
-  let Difference_In_Days = Math.floor(
+  const differenceInDays = Math.floor(
     (date2.getTime() - date1.getTime()) / oneDay
   );
 
-  return Difference_In_Days;
+  return differenceInDays;
 };
