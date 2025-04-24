e79f4cadf4510d190f8cc4de06a16d253501f96c
MayHyeyeonKim <hkim7963@gmail.com>
2025-04-24 16:12:37 -0700

    refactor: move MUI ThemeProvider to Providers component

--- src/providers/Providers.tsx
+++ src/providers/Providers.tsx
@@ -1,7 +1,11 @@
+/**
+ *  Providers.tsx
+ *
+ *  @copyright 2024 Digital Aid Seattle
+ *
+ */
 import { ReactNode } from "react";
-
 import { FilterContextProvider } from "../context/FilterContext.tsx";
-
 import { HospitalsContextProvider } from "../context/HospitalContext.tsx";
 import {
   DonationHospitalContextProvider,
@@ -11,26 +15,32 @@ import {
 import { DonationContextProvider } from "../context/DonationContext.tsx";
 import { DrawerWidthContextProvider } from "../context/DrawerWidthContext.tsx";
 import { GeneralInfoContextProvider } from "../context/GeneralInfoContext.tsx";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import Palette from "../styles/theme";
 
+const themes = Palette();
 type ProvidersProps = { children: ReactNode };
 
 const Providers = ({ children }: ProvidersProps) => {
   return (
-    <GeneralInfoContextProvider>
-      <DrawerWidthContextProvider>
-        <DonationContextProvider>
-          <HospitalsContextProvider>
-            <SelectedHospitalContextProvider>
-              <DonationHospitalContextProvider>
-                <LearnMoreHospitalContextProvider>
-                  <FilterContextProvider>{children}</FilterContextProvider>
-                </LearnMoreHospitalContextProvider>
-              </DonationHospitalContextProvider>
-            </SelectedHospitalContextProvider>
-          </HospitalsContextProvider>
-        </DonationContextProvider>
-      </DrawerWidthContextProvider>
-    </GeneralInfoContextProvider>
-  )
+    <ThemeProvider theme={themes}>
+      <CssBaseline />
+      <GeneralInfoContextProvider>
+        <DrawerWidthContextProvider>
+          <DonationContextProvider>
+            <HospitalsContextProvider>
+              <SelectedHospitalContextProvider>
+                <DonationHospitalContextProvider>
+                  <LearnMoreHospitalContextProvider>
+                    <FilterContextProvider>{children}</FilterContextProvider>
+                  </LearnMoreHospitalContextProvider>
+                </DonationHospitalContextProvider>
+              </SelectedHospitalContextProvider>
+            </HospitalsContextProvider>
+          </DonationContextProvider>
+        </DrawerWidthContextProvider>
+      </GeneralInfoContextProvider>
+    </ThemeProvider>
+  );
 };
 export default Providers;
